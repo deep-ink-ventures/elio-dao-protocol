9e458550f85ac5f2650efe5cd9e27d3352996ae5
deep-ink-ventures <chp@deep-ink.ventures>
2023-05-11 09:27:03 +0200

    cleanup

--- init.sh
+++ init.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-echo "> Compiling asset contract ..."
+printf "> Compiling asset contract ..."
 cd contracts/assets && cargo build --target wasm32-unknown-unknown --release && cd ../..
 cp contracts/assets/target/wasm32-unknown-unknown/release/elio_assets.wasm wasm
 
-echo "> Compiling votes contract ..."
+printf "> Compiling votes contract ..."
 cd contracts/votes && cargo build --target wasm32-unknown-unknown --release && cd ../..
 cp contracts/votes/target/wasm32-unknown-unknown/release/elio_votes.wasm wasm
 
-echo "> Compiling core contract ..."
+printf "> Compiling core contract ..."
 cd contracts/core && cargo build --target wasm32-unknown-unknown --release && cd ../..
 cp contracts/core/target/wasm32-unknown-unknown/release/elio_core.wasm wasm
