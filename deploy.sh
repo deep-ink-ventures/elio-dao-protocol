97394334ee9d3ab22f79b574f02e2f8c8ab445a1
deep-ink-ventures <chp@deep-ink.ventures>
2023-05-11 08:58:38 +0200

    added deploy

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+source .env
+
+printf "\nDeploying votes ...\n"
+
+soroban contract deploy \
+    --wasm wasm/elio_votes.wasm \
+    --source ${SECRET_KEY} \
+    --rpc-url ${RPC_URL} \
+    --network-passphrase "${NETWORK_PASSPHRASE}"
+
+
+printf "\nDeploying core ...\n"
+
+soroban contract deploy \
+    --wasm wasm/elio_votes.wasm \
+    --source ${SECRET_KEY} \
+    --rpc-url ${RPC_URL} \
+    --network-passphrase "${NETWORK_PASSPHRASE}"
+
+
+printf "\nDeploying assets ...\n"
+
+soroban contract deploy \
+    --wasm wasm/elio_votes.wasm \
+    --source ${SECRET_KEY} \
+    --rpc-url ${RPC_URL} \
+    --network-passphrase "${NETWORK_PASSPHRASE}"
