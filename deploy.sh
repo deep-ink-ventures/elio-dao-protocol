1e5b03d1efa8660ee51ad4652aba081cae4cba97
deep-ink-ventures <chp@deep-ink.ventures>
2023-05-11 08:59:10 +0200

    fixed typo

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ soroban contract deploy \
 printf "\nDeploying core ...\n"
 
 soroban contract deploy \
-    --wasm wasm/elio_votes.wasm \
+    --wasm wasm/elio_core.wasm \
     --source ${SECRET_KEY} \
     --rpc-url ${RPC_URL} \
     --network-passphrase "${NETWORK_PASSPHRASE}"
@@ -23,7 +23,7 @@ soroban contract deploy \
 printf "\nDeploying assets ...\n"
 
 soroban contract deploy \
-    --wasm wasm/elio_votes.wasm \
+    --wasm wasm/elio_assets.wasm \
     --source ${SECRET_KEY} \
     --rpc-url ${RPC_URL} \
     --network-passphrase "${NETWORK_PASSPHRASE}"
