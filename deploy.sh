52e56c7af9aaaed7b125b20802ff94f6bdffc948
shirku <robin.barbulla@gmail.com>
2023-06-06 17:14:10 +0200

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,24 +4,29 @@ source .env
 
 printf "\nDeploying core ...\n"
 
+CONTRACT_ID="$(
 soroban contract deploy \
     --wasm wasm/elio_core.wasm \
-    --source ${SECRET_KEY} \
-    --rpc-url ${RPC_URL} \
+    --source "${SECRET_KEY}" \
+    --rpc-url "${RPC_URL}" \
     --network-passphrase "${NETWORK_PASSPHRASE}"
-
-
+)"
+export CONTRACT_ID
 printf "\nInstalling assets ...\n"
-
+ASSETS_WASM_HASH="$(
 soroban contract install \
     --wasm wasm/elio_assets.wasm \
     --source ${SECRET_KEY} \
     --rpc-url ${RPC_URL} \
     --network-passphrase "${NETWORK_PASSPHRASE}"
-
+)"
+export ASSETS_WASM_HASH
 printf "\nInstalling votes ...\n"
+VOTES_WASM_HASH="$(
 soroban contract install \
     --wasm wasm/elio_votes.wasm \
     --source ${SECRET_KEY} \
     --rpc-url ${RPC_URL} \
     --network-passphrase "${NETWORK_PASSPHRASE}"
+)"
+export VOTES_WASM_HASH
