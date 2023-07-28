d71eee3fd4aa087cb82c2bdb93f972b9683fb6fe
deepink-pat <pap@deep-ink.ventures>
2023-07-28 15:51:47 +0800

    created all hookpoints

--- contracts/hookpoints/src/interface.rs
+++ contracts/hookpoints/src/interface.rs
@@ -14,5 +14,16 @@ pub trait HookpointsTrait {
     ///
     /// - `dao_id`: The dao id that has been voted for
     /// - `proposal_owner`: The owner of the proposal
-    fn on_before_proposal_creation(_env: Env, _dao_id: Bytes, _proposal_owner: Address);
+    fn on_before_proposal_creation(env: Env, dao_id: Bytes, proposal_owner: Address);
+    fn on_before_set_metadata(env:Env, dao_id: Bytes, proposal_id: u32, meta: Bytes, hash: Bytes, proposal_owner: Address);
+    fn on_set_configuration(env: Env, dao_id: Bytes, proposal_duration: u32, proposal_token_deposit: u128) -> (u32, u128);
+    fn on_before_fault_proposal(env: Env, dao_id: Bytes, proposal_id: u32, reason: Bytes);
+    fn on_before_finalize_proposal(env: Env, dao_id: Bytes, proposal_id: u32);
+    fn on_before_mark_implemented(env: Env, proposal_id: u32);
+    fn on_before_dao(env: Env, dao_id: Bytes);
+    fn on_before_change_owner(env: Env, dao_id: Bytes);
+    fn on_incr_allowance(env: Env, from: Address, spender: Address, amount: i128) -> i128;
+    fn on_decr_allowance(env: Env, from: Address, spender: Address, amount: i128) -> i128;
+    fn on_xfer(env: Env, from: Address, to: Address, amount: i128) -> i128;
+    fn on_xfer_from(env: Env, spender: Address, from: Address, to: Address, amount: i128) -> i128;
 }
--- contracts/hookpoints/src/lib.rs
+++ contracts/hookpoints/src/lib.rs
@@ -28,12 +28,58 @@ pub struct HookpointsContract;
 
 #[contractimpl]
 impl HookpointsTrait for HookpointsContract {
+    /* Core HookPoints */
+    fn on_before_dao(_env: Env, _dao_id: Bytes) {
+        // add functionality here
+    }
 
+    fn on_before_change_owner(_env: Env, _dao_id: Bytes) {
+        // add functionality here
+    }
+
+    /* Votes HookPoints */
     fn on_vote(_env: Env, _dao_id: Bytes, _proposal_id: u32, _account_id: Address, amount: i128) -> i128 {
         amount
     }
 
     fn on_before_proposal_creation(_env: Env, _dao_id: Bytes, _proposal_owner: Address) {
         // add functionality here
+    }
+
+    fn on_before_set_metadata(_env: Env, _dao_id: Bytes, _proposal_id: u32, _meta: Bytes, _hash: Bytes, _proposal_owner: Address) {
+        // add functionality here
+    }
+
+    fn on_set_configuration(_env: Env, _dao_id: Bytes, proposal_duration: u32, proposal_token_deposit: u128) -> (u32, u128) {
+        (proposal_duration, proposal_token_deposit)
+    }
+
+    fn on_before_fault_proposal(_env: Env, _dao_id: Bytes, _proposal_id: u32, _reason: Bytes) {
+        // add functionality here
+    }
+
+    fn on_before_finalize_proposal(_env: Env, _dao_id: Bytes, _proposal_id: u32) {
+        // add functionality here
+    }
+
+    fn on_before_mark_implemented(_env: Env, _proposal_id: u32) {
+        // add functionality here
+    }
+
+    /* Assets HookPoints */
+    fn on_incr_allowance(_env: Env, _from: Address, _spender: Address, amount: i128) -> i128 {
+        amount
+    }
+
+    fn on_decr_allowance(_env: Env, _from: Address, _spender: Address, amount: i128) -> i128 {
+        amount
+    }
+
+    fn on_xfer(_env: Env, _from: Address, _to: Address, amount: i128) -> i128 {
+        amount
+    }
+
+    fn on_xfer_from(_env: Env, _spender: Address, _from: Address, _to: Address, amount: i128) -> i128 {
+        amount
     }
 }
